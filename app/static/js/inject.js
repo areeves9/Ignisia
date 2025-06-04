a775ceee0f876a8a067d0f1d81f1c756385bb779
areeves9 <areeves9@icloud.com>
2025-06-03 23:42:58 -0700

    csrf tokens and template dir

--- app/static/js/inject.js
+++ app/static/js/inject.js
@@ -1,18 +1,4 @@
 export async function injectComponentsAndInitAlpine() {
-  const [navbar, login, register, simulator, splash] = await Promise.all([
-    fetch('/static/components/navbar.html').then((r) => r.text()),
-    fetch('/static/components/login.html').then((r) => r.text()),
-    fetch('/static/components/register.html').then((r) => r.text()),
-    fetch('/static/components/simulator.html').then((r) => r.text()),
-    fetch('/static/components/splash.html').then((r) => r.text()),
-  ]);
-
-  document.getElementById('navbar-slot').innerHTML = navbar;
-  document.getElementById('login-slot').innerHTML = login;
-  document.getElementById('register-slot').innerHTML = register;
-  document.getElementById('simulator-slot').innerHTML = simulator;
-  document.getElementById('splash-slot').innerHTML = splash;
-
   Alpine.initTree(document.body);
 
   Alpine.store('auth').init();
